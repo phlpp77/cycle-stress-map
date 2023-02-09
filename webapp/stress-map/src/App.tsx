2ac327419a33bf724bc98fd186350606b19206e9
phlpp77 <PhilippHemkemeyer@iCloud.com>
2023-02-09 18:28:56 -0500

    Add leaflet map of ATL

--- webapp/stress-map/src/App.tsx
+++ webapp/stress-map/src/App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { Icon } from "leaflet";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h2>
+          <b>Stress Map</b> <code>[unstable prototype]</code>
+        </h2>
+        <MapContainer
+          center={[33.753746, -84.38633]}
+          zoom={13}
+          scrollWheelZoom={true}
         >
-          Learn React
-        </a>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </MapContainer>
       </header>
     </div>
   );
