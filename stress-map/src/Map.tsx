15af97957645bf04152d574c72faadf44517b9c8
phlpp77 <PhilippHemkemeyer@iCloud.com>
2023-02-10 12:41:27 -0500

    Show note markes on map

--- stress-map/src/Map.tsx
+++ stress-map/src/Map.tsx
@@ -1,14 +1,38 @@
 import "./Map.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
+// TODO: type not complete
+type note = {
+  user_id: number;
+  trip_id: string;
+  latitude: number;
+  longitude: number;
+};
+
 function Map(): JSX.Element {
+  const [coords, setCoords] = useState<note[]>([]);
+
+  useEffect(() => {
+    const fetchAllCoords = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/note");
+        setCoords(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAllCoords();
+  }, []);
+
   return (
     <div className="Map">
       <header className="Map-header">
         <h2>
           <b>Stress Map</b> <code>[unstable prototype]</code>
         </h2>
+
         <MapContainer
           center={[33.753746, -84.38633]}
           zoom={13}
@@ -18,6 +42,11 @@ function Map(): JSX.Element {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           />
+          
+          {/* Placing markers at note positions */}
+          {coords.map((coord) => (
+            <Marker position={[coord.latitude, coord.longitude]}></Marker>
+          ))}
         </MapContainer>
       </header>
     </div>
