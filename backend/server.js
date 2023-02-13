78bdfcdcbf532dd5613e8a4ef7032cc6a4ed0463
phlpp77 <PhilippHemkemeyer@iCloud.com>
2023-02-13 17:32:26 -0500

    Add get trip with trip_id to backend

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,18 @@ app.get("/note", (req, res) => {
   });
 });
 
+// Get trip with certain trip_id
+app.get("/trip", (req, res) => {
+  // Query to get all lat and long data from coords with matching trip_id
+  const query = "SELECT latitude, longitude FROM coord WHERE trip_id=76;";
+
+  // Send query to db connection
+  db.query(query, (err, data) => {
+    if (err) return res.json(err); // When error occurs send client error code
+    return res.json(data);
+  });
+});
+
 app.listen(8800, () => {
   console.log("Connection to backend successful.");
 });
